81b8c99fddcfcea6ef6d5a7ed8a1ce3264ccc65e
Hulnis <eric.parker13@gmail.com>
2018-02-03 13:28:24 -0500

    basic input output

--- lib/calc.ex
+++ lib/calc.ex
@@ -19,5 +19,22 @@ defmodule Calc do
     line
     |> String.trim()
     |> String.split()
+    |> basic_math()
+  end
+
+  @doc """
+  Does one math operation
+  """
+  def basic_math(args) do
+    num1 = args[0]
+    op = args[1]
+    num2 = args[2]
+    case op do
+      "+" -> num1 + num2
+      "-" -> num1 - num2
+      "/" -> num1 / num2
+      "*" -> num1 * num2
+      + -> "Error"
+    end
   end
 end
