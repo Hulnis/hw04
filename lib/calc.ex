dc3c014ad0b0beaab3b33a74c327b8c7746fa916
Hulnis <eric.parker13@gmail.com>
2018-02-03 16:49:11 -0500

    basic input output[B

--- lib/calc.ex
+++ lib/calc.ex
@@ -16,37 +16,36 @@ defmodule Calc do
   parse through input once paranths have been dealt with, turning into prefix list using turn_to_prefix
   """
   def parse_input(input) do
-    split_string = input
+    post = input
     |> String.trim()
     |> String.split()
+    |> turn_to_postfix(0, [], [])
 
-    pre = turn_to_prefix(split_string ++ [")"] ,0, ["("], [])
-
-    IO.inspect(pre)
+    IO.inspect(post)
   end
 
   @doc """
-  Loops over array, creating a ternary structure of op, left, and right
+  Loops over array, creating a postfix expressions
   1 + 2 * 3 + 4
   """
-  def turn_to_prefix(array, index, op_stack, num_stack) when index < Kernel.length(array) do
+  def turn_to_postfix(array, index, op_stack, output) when index < Kernel.length(array) do
     next_elem = Enum.at(array, index)
     IO.inspect(next_elem)
     IO.inspect(op_stack)
-    IO.inspect(num_stack)
+    IO.inspect(output)
     IO.puts("-----------")
     if next_elem == "(" do
-      turn_to_prefix(array, index + 1, op_stack ++ ["("], num_stack)
+      turn_to_prefix(array, index + 1, stack ++ ["("], output)
     end
     if next_elem == ")" do
-      result = match_paranths(op_stack, num_stack)
+      result = match_paranths(op_stack, output)
       turn_to_prefix(array, index + 1, elem(result, 0), elem(result, 1))
     end
     if next_elem == "+" || next_elem == "-" || next_elem == "*" || next_elem == "/" do
-      result = pop_ops(op_stack, num_stack, next_elem)
+      result = pop_ops(op_stack, output, next_elem)
       turn_to_prefix(array, index + 1, elem(result, 0), elem(result, 1))
     else
-      turn_to_prefix(array, index + 1, op_stack, num_stack ++ [next_elem])
+      turn_to_prefix(array, index + 1, op_stack, output ++ [next_elem])
     end
   end
 
@@ -54,53 +53,36 @@ defmodule Calc do
   Loops over array, creating a ternary structure of op, left, and right
   1 + 2 * 3 + 4
   """
-  def turn_to_prefix(array, index, op_stack, num_stack) when index >= Kernel.length(array) do
-    {op_stack, num_stack}
+  def turn_to_postfix(array, index, op_stack, output) when index >= Kernel.length(array) do
+    empty_stack(op_stack, 0, output)
   end
 
-  @doc """
-  pops elemnts from op_stack until we find close paranth
-  """
-  def match_paranths(op_stack, num_stack) do
-    IO.puts("adding at end")
-    IO.inspect(num_stack)
-    IO.inspect(op_stack)
-    if List.last(op_stack) != "(" do
-      op = List.last(op_stack)
-      num2 = List.last(num_stack)
-      num_stack = List.delete(num_stack, num2)
-      num1 = List.last(num_stack)
-      num_stack = List.delete(num_stack, num1)
-      match_paranths(List.delete(op_stack, op), num_stack ++ [op, num1, num2])
-    else
-      IO.puts("in else")
-      IO.inspect(num_stack)
-      op_stack = List.delete(op_stack, List.last(op_stack))
-      {op_stack, num_stack}
+  def match_paranths(op_stack, output) do
+    next_elem = List.last(op_stack)
+    if next_elem != "(" do
+      match_paranths(List.delete(op_stack, next_elem), output ++ [next_elem])
+    end
+      {op_stack, output}
+  end
+
+  def pop_ops(op_stack, output, op) do
+    if Kernel.length(op_stack) == 0 do
+      next_elem = List.last(op_stack)
+      if op == * || op == / do
+        {List.delete(op_stack, op), output ++ [op]}
+      else
+        result pop_ops(List.delete(op_stack, next_elem), output, op)
+        {op_stack ++ [next_elem], output}
+      end
     end
   end
 
-  @doc """
-  Pops elements until finds an operator that comes after
-  """
-  def pop_ops(op_stack, num_stack, op) when Kernel.length(op_stack) >= 0 do
-    next_op = List.last(op_stack)
-
-    if next_op == "*" || next_op == "/" do
-      num2 = List.last(num_stack)
-      num_stack = List.delete(num_stack, num2)
-      num1 = List.last(num_stack)
-      num_stack = List.delete(num_stack, num1)
-      pop_ops(List.delete(op_stack, next_op), num_stack ++ [op, num1, num2])
-    else
-      {op_stack ++ [op], num_stack}
-    end
+  def empty_stack(stack, index, output) when index < Kernel.length(array) do
+    next_elem = List.last(stack)
+    empty_stack(List.delete(next_elem), index + 1, output ++ [next_elem])
   end
-  @doc """
-  Pops elements until finds an operator that comes after base case of empty stack
-  """
-  def pop_ops(op_stack, num_stack) when Kernel.length(op_stack) == 0 do
-    op_stack = op_stack.delete(List.last(op_stack))
-    {op_stack, num_stack}
+
+  def empty_stack(stack, index, output) when index >= Kernel.length(array) do
+    output
   end
 end
