11b667ed3304ede824771711bdcab92999218767
Eric Parker <parker.er@husky.neu.edu>
2018-02-03 23:33:32 +0000

    we in there

--- lib/calc.ex
+++ lib/calc.ex
@@ -4,132 +4,15 @@ defmodule Calc do
   """
 
   @doc """
-  Repeatedly prints a prompt and reads input, sends to eval
+  Hello world.
+
+  ## Examples
+
+      iex> Calc.hello
+      :world
+
   """
-  def main do
-    input = IO.gets("Input your expression to be evaluated: ")
-    eval_expr(input)
-    main()
-  end
-
-  @doc """
-  parse through input once paranths have been dealt with, turning into postfix list using turn_to_postfix
-  """
-  def eval_expr(input) do
-    post = input
-    |> String.trim()
-    |> String.replace("(", " ( ")
-    |> String.replace(")", " ) ")
-    |> String.split(" ",  trim: true)
-    |> turn_to_postfix(0, [], [])
-    |> eval_postfix([], 0)
-
-    IO.inspect(post)
-  end
-
-  @doc """
-  Loops over array, creating a postfix expressions
-  1 + 2 * 3 + 4
-  """
-  def turn_to_postfix(array, index, op_stack, output) when index < Kernel.length(array) do
-    next_elem = Enum.at(array, index)
-    cond do
-      next_elem == "(" ->
-        turn_to_postfix(array, index + 1, op_stack ++ ["("], output)
-      next_elem == ")" ->
-        result = match_paranths(op_stack, output)
-        turn_to_postfix(array, index + 1, elem(result, 0), elem(result, 1))
-      next_elem == "+" || next_elem == "-" || next_elem == "*" || next_elem == "/" ->
-        result = pop_ops(op_stack, output, next_elem)
-        turn_to_postfix(array, index + 1, elem(result, 0), elem(result, 1))
-      true ->
-        turn_to_postfix(array, index + 1, op_stack, output ++ [elem(Float.parse(next_elem), 0)])
-    end
-  end
-
-  @doc """
-  Loops over array, creating a ternary structure of op, left, and right
-  1 + 2 * 3 + 4
-  """
-  def turn_to_postfix(array, index, op_stack, output) when index >= Kernel.length(array) do
-    empty_stack(op_stack, output)
-  end
-
-  def match_paranths(op_stack, output) do
-    next_elem = List.last(op_stack)
-    if next_elem != "(" do
-      match_paranths(List.delete(op_stack, next_elem), output ++ [next_elem])
-    else
-      {List.delete(op_stack, next_elem), output}
-    end
-  end
-
-  def pop_ops(op_stack, output, op) do
-    if Kernel.length(op_stack) > 0 do
-      next_elem = List.last(op_stack)
-      if op == "*" || op == "/" || next_elem == "(" do
-        {op_stack ++ [op], output}
-      else
-        result = pop_ops(List.delete(op_stack, next_elem), output, op)
-        op_stack = elem(result, 0)
-        output = elem(result, 1)
-        {op_stack ++ [next_elem], output}
-      end
-    else
-      {op_stack ++ [op], output}
-    end
-  end
-
-  def empty_stack(stack, output) when Kernel.length(stack) > 0 do
-    next_elem = List.last(stack)
-    empty_stack(List.delete(stack, next_elem), output ++ [next_elem])
-  end
-
-  def empty_stack(stack, output) when Kernel.length(stack) == 0 do
-    output
-  end
-
-  def eval_postfix(postfix, stack, index) when index < Kernel.length(postfix) do
-    next_elem = Enum.at(postfix, index)
-    cond do
-      next_elem == "+" ->
-        result = get_two_elem(stack)
-        num1 = result[:num1]
-        num2 = result[:num2]
-        stack = result[:stack]
-        eval_postfix(postfix, stack ++ [num1 + num2], index + 1)
-      next_elem == "-" ->
-        result = get_two_elem(stack)
-        num1 = result[:num1]
-        num2 = result[:num2]
-        stack = result[:stack]
-        eval_postfix(postfix, stack ++ [num1 - num2], index + 1)
-      next_elem == "*" ->
-        result = get_two_elem(stack)
-        num1 = result[:num1]
-        num2 = result[:num2]
-        stack = result[:stack]
-        eval_postfix(postfix, stack ++ [num1 * num2], index + 1)
-      next_elem == "/" ->
-        result = get_two_elem(stack)
-        num1 = result[:num1]
-        num2 = result[:num2]
-        stack = result[:stack]
-        eval_postfix(postfix, stack ++ [num1 / num2], index + 1)
-      true ->
-        eval_postfix(postfix, stack ++ [next_elem], index + 1)
-    end
-  end
-
-  def eval_postfix(postfix, stack, index) when index >= Kernel.length(postfix) do
-    Enum.at(stack, 0)
-  end
-
-  def get_two_elem(stack) do
-    num2 = List.last(stack)
-    stack = List.delete(stack, num2)
-    num1 = List.last(stack)
-    stack = List.delete(stack, num1)
-    %{:num1 => num1, :num2 => num2, :stack => stack}
+  def hello do
+    :world
   end
 end
--- test/calc_test.exs
+++ test/calc_test.exs
@@ -2,31 +2,7 @@ defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
 
-  test "basic test" do
-    assert Calc.eval_expr("1 + 2") == 3
-    assert Calc.eval_expr("1 - 2") == -1
-    assert Calc.eval_expr("1 * 2") == 2
-    assert Calc.eval_expr("1 / 2") == 0.5
-    assert Calc.eval_expr("1 + 2 * 3") == 7
-    assert Calc.eval_expr("(1 + 2) * 3") == 9
-  end
-
-  test "turn to postfix" do
-    assert Calc.turn_to_postfix(["1", "+", "2"], 0, [], []) == [1, 2, "+"]
-    assert Calc.turn_to_postfix(["1", "-", "2"], 0, [], []) == [1, 2, "-"]
-    assert Calc.turn_to_postfix(["1", "*", "2"], 0, [], []) == [1, 2, "*"]
-    assert Calc.turn_to_postfix(["1", "/", "2"], 0, [], []) == [1, 2, "/"]
-    assert Calc.turn_to_postfix(["1", "+", "2", "*", "3"], 0, [], []) == [1, 2, 3, "*", "+"]
-    assert Calc.turn_to_postfix(["(", "1", "+", "2", ")", "*", "3"], 0, [], []) == [1, 2, "+", 3, "*"]
-  end
-
-  test "eval postfix" do
-    assert Calc.eval_postfix([1, 2, "+"], [], 0) == 3
-    assert Calc.eval_postfix([1, 2, "-"], [], 0) == -1
-    assert Calc.eval_postfix([1, 2, "/"], [], 0) == 0.5
-    assert Calc.eval_postfix([1, 2, "*"], [], 0) == 2
-    assert Calc.eval_postfix([1, 2, 3, "*", "+"], [], 0) == 7
-    assert Calc.eval_postfix([1, 2, "+", 3, "*"], [], 0) == 9
-
+  test "greets the world" do
+    assert Calc.hello() == :world
   end
 end
